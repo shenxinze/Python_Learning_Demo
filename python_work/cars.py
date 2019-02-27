d5d300a665a9e21714fdae75dca5c363c6f6a2ce
shendekuan <uidshen@163.com>
2019-02-27 17:58:22 +0800

    Python 学习

---
+++ python_work/cars.py
@@ -0,0 +1,22 @@
+cars = ['bmw','audi','toyota','subaru']
+# cars.sort()
+# print(cars)
+
+# cars.sort(reverse = True)
+# print(cars)
+
+# print('Here is the original list:')
+# print(cars)
+
+# print('\nHere is the sorted list:')
+# print(sorted(cars))
+
+# print('\nHere is the ogiginal list again:')
+# print(cars)
+
+# print(cars)
+# cars.reverse()
+# print(cars)
+
+length = len(cars)
+print(length)
---
+++ python_work/test_2.py
@@ -0,0 +1,16 @@
+cities = ['三亚','马尔代夫','澳大利亚','新加坡','巴厘岛']
+print(cities)
+print(sorted(cities))
+print(cities)
+print(sorted(cities,reverse = True))
+print(cities)
+cities.reverse()
+print(cities)
+cities.reverse()
+print(cities)
+cities.sort(reverse = True)
+print(cities)
+
+print(len(cities))
+
+# print(cities[5])
