a51d1b12d7dd107cb5b327a76a153e0332e92411
shendekuan <uidshen@163.com>
2019-02-28 10:42:03 +0800

    调整目录

---
+++ README.md
@@ -0,0 +1,3 @@
+# Python_Learning_Demo
+
+> 记录《Python编程从入门到实践》书中的练习
---
+++ test_3.py
@@ -0,0 +1,10 @@
+nums = [1,2,3]
+for num in nums:
+	print('My favorite number is ' + str(num) + '.')
+print('I like all these Numbers\n')
+
+pets = ['cat','dog','squirrel']
+for pet in pets:
+	# print(pet)
+	print('A ' + pet + ' would make a great pet')
+print('Any of these animals would make a great pet!')
